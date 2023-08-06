567b0212c8573fd90bb291dd2998aba6b1369f54
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-08-06 19:10:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function reducer(batteryBatches) {
+    return batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+  
+  const totalBatteries = reducer(batteryBatches);
+  console.log(totalBatteries);
